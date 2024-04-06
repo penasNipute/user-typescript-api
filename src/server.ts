9eb5fa3a2bb2269ba255c981142b7f3805cb63f4
penasNipute <penaswild@gmail.com>
2024-04-07 01:25:02 +0200

    servidor criado com fastify e BD sqlite com prisma

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,21 @@
-console.log('Penas...')
+import { error } from 'console'
+import fastify from 'fastify'
+import { config } from 'dotenv'
+import { parse } from 'path'
+
+config()
+const PORT = parseInt(process.env.PORT as string)
+const app = fastify()
+
+app.get('/',(req,res) => {
+  return res.send({message:"Ola amigo... Ola amigo!"})
+})
+
+
+app.listen({port:PORT})
+.then(() => {
+  console.log(`Server running on port http://localhost:${PORT}`)
+})
+.catch((error) => {
+  console.log({error:error.message})
+})
